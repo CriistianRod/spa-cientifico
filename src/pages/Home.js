3c2b6932fa2a0efc638c074f4be217b73af64605
Cristian <cristian.frontend.dev@gmail.com>
2021-07-07 10:17:14 -0500

    styles added getData util created wepback modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-const Home = () => {
+import getData from '../utils/getData'
+
+const Home =  async () => {
+    const characters = await getData()
+
     const view = /*html*/`
+    <h1>Total resultados: ${characters.results.length}</h1>
     <section class="Characters">
+    ${characters.results.map(character => /*html*/`
         <article class="Character-item">
-            <a href="#/1/">
-                <img src="image" alt="name"/>
-                <h2></h2>
+            <p>${character.id}</p>
+            <a href="#/${character.id}/">
+                <img src=${character.image} alt=${character.name}/>
+                <h2>${character.name}</h2>
             </a>
         </article>
+    `).join('')}
     </section>
     `
 
