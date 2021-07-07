2a88393c79c6c364ed65788dc066b18549e6a29c
Cristian <cristian.frontend.dev@gmail.com>
2021-07-07 08:37:51 -0500

    routes, utils, error page included

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+    const view = /*Html*/`
+        <div class="Error404">
+            <h2>Error 404</h2>
+        </div>
+    `
+
+    return view
+}
+
+export default Error404
